389654bca85589917fc7de1c49f0b70c30304215
Romario de Melo Damasceno <romario.auth@gmail.com>
2020-11-17 06:25:18 -0300

    Consume Contract Create: add search

--- routes/contracts.js
+++ routes/contracts.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const sequelize = require('../db/connection')
 const router = express.Router()
 const Contract = require('../models/Contract')
 const Article = require('../models/Article')
 const Event = require('../models/Event')
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 
 // Get 
@@ -31,25 +32,68 @@ router.get('/manager/event/:idEvent', (req, res) => {
 // Get value dataEvent
 router.get('/event/:idEvent', (req, res) => {
     let idEvent = req.params.idEvent
+    let termSearch = req.query.term
 
-    if(idEvent > 0)
-    {
-        res.render('eventManager', {idEvent});
-    }else
-    {
-        res.redirect('/notfound');
-    }
+    let query = '%' + termSearch + '%'
+               
+    Event.findByPk(idEvent)
+    .then((event) => {
+
+        if(event === null)
+        {
+            res.redirect('/notfound');
+        }else
+        {
+            // Give back Search if term
+            if(!termSearch)
+            {
+                console.log("\n\n ****Devolver o Evento: " + req.path)
+                res.render('eventManager', {event});
+            }
+            else{
+                Article.findAll({
+                    where: {
+                        description: {[Op.like]: query}
+                    }
+                })
+                .then((resultSearch) => {
+                    res.render('eventManager', {event,termSearch, resultSearch});
+                    console.log("\n\n ****Fazer busca 1: " + req.path)
+                })
+                .catch((err) => console.log("Error search Article"))
+            }
+        }
+    })
+    .catch((err) => console.log("Erro get Event for Manager contract"))
+
 })
 
+
+
+// Add Contract
+router.post('/article/add/', (req, res) => {
+    let idEvent = req.body.idEvent
+    let idArticle = req.body.idArticle
+    let valueContract = req.body.valueContract
+
+    // Insert
+    Contract.create({
+        event_id: idEvent,
+        article_id: idArticle,
+        value: valueContract
+    })
+    .then(() => res.redirect('/aplicativo/contract/event/'+idEvent))
+    .catch(err => console.log('Erro add Contract: ' + err))
+})
 
 // Delete Contract
 router.post('/article/delete/', (req, res) => {
     
-    let idContract = req.body.idContract;
-    let idEvent = req.body.idEvent;
+    let idEvent = req.body.idEvent;    
+    let idArticle = req.body.idArticle;
     
     Contract.destroy({
-        where: {id: idContract}
+        where: {event_id: idEvent, article_id: idArticle}
     })
     .then(() => res.redirect('/aplicativo/contract/event/'+idEvent))
     .catch(err => console.log('Delete erro' + err))
